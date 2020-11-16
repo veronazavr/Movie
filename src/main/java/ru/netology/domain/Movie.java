8eb833146ab14e52967e3d186f8879b13aac82b2
Veronazavr <verochka10@list.ru>
2020-11-16 16:56:28 +0300

    added updated

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class Movie {
+    private int id;
+    private int productId;
+    private String productName;
+    private int productPrice;
+    private int count;
+
+}
---
+++ src/main/java/ru/netology/domain/manager/MovieManager.java
@@ -0,0 +1,12 @@
+package ru.netology.domain.manager;
+
+import ru.netology.domain.Movie;
+
+public class MovieManager {
+    private Movie[] movies;
+
+    public Movie[] getMovies() {
+        //TODO: add logic
+        return null;
+    }
+}

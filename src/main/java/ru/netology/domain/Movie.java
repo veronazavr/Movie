41c0c436dacea3d7868e41cafe5158dd0aceaa2a
Veronazavr <verochka10@list.ru>
2020-11-19 21:01:08 +0300

    added branch

--- src/main/java/ru/netology/domain/Movie.java
+++ src/main/java/ru/netology/domain/Movie.java
@@ -2,9 +2,14 @@ package ru.netology.domain;
 
 public class Movie {
     private int id;
-    private int productId;
-    private String productName;
-    private int productPrice;
-    private int count;
+    private String imageUrl;
+    private String name;
+    private String genre;
 
-}
+    public Movie(int id, String imageUrl, String name, String genre) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.genre = genre;
+    }
+    }
--- src/main/java/ru/netology/domain/manager/MovieManager.java
+++ src/main/java/ru/netology/domain/manager/MovieManager.java
@@ -11,27 +11,30 @@ public class MovieManager {
     }
 
     public MovieManager() {
+
     }
 
-    public void addMovie(Movie addedMovie) {
-        int length = movies.length + 1;
-        Movie[] tmp = new Movie[length];
-        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        public void addMovie (Movie addedMovie){
+            int length = movies.length + 1;
+            Movie[] tmp = new Movie[length];
+            System.arraycopy(movies, 0, tmp, 0, movies.length);
 
-        int lastMovieIndex = tmp.length - 1;
-        tmp[lastMovieIndex] = addedMovie;
-        movies = tmp;
-    }
-
-    public Movie[] getLastMovies() {
-        int resultLength = Math.min(afishaLenght, movies.length);
-
-        Movie[] result = new Movie[resultLength];
-
-        for (int i = 0; i < resultLength; i++) {
-            int index = movies.length - i - 1;
-            result[i] = movies[index];
+            int lastMovieIndex = tmp.length - 1;
+            tmp[lastMovieIndex] = addedMovie;
+            movies = tmp;
         }
-        return result;
+
+        public Movie[] getLastMovies () {
+            int resultLength = Math.min(afishaLenght, movies.length);
+
+            Movie[] result = new Movie[resultLength];
+
+            for (int i = 0; i < resultLength; i++) {
+                int index = movies.length - i - 1;
+                result[i] = movies[index];
+            }
+            return result;
+        }
     }
-}
+
+
